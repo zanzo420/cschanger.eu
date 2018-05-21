8c8eba7c05950978eb63e874695921adb3302557
Kamil <32514182+tejkuu@users.noreply.github.com>
2018-05-21 22:18:28 +0200

    Add files via upload

---
+++ CSChanger_client/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CSChanger")>
+<Assembly: AssemblyDescription("CSChanger.eu")>
+<Assembly: AssemblyCompany("CSChanger.eu")>
+<Assembly: AssemblyProduct("CSChanger")>
+<Assembly: AssemblyCopyright("Copyright © CSChanger.eu  2017")>
+<Assembly: AssemblyTrademark("CSChanger.eu 2017")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ef6498a7-108c-4f0d-9949-a8eb069e2066")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.00.2.1")>
+<Assembly: AssemblyFileVersion("1.00.2.1")>
